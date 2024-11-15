9d71b33d35a11a0886ac816f80355bd6bbca55f5
Liam Ramirez-Guess <mountnRocky@gmail.com>
2024-11-15 10:57:09 -0700

    Added databse line for future referance

--- Serve.py
+++ Serve.py
@@ -1,5 +1,8 @@
 import json
 
+# Database url. It will later fetch from this to always have an updated databse to grab from
+url = "https://raw.githubusercontent.com/LRGuess/potential-enigma/main/database/riddles.json"
+
 with open('database/riddles.json') as r:
   riddles = json.load(r)
 
